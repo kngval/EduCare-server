f79713eb9b98901e9ce24e018fb81afd18c726b6
kngval <bkvalteocs@gmail.com>
2024-10-01 21:19:41 +0800

    Database Context

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SMSDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
 
 var app = builder.Build();
 
