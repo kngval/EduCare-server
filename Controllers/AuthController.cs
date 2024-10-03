47b7167064a928013f76fd97e3b491c6ab1d5051
kngval <bkvalteocs@gmail.com>
2024-10-03 16:38:11 +0800

    error handling

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -5,28 +5,39 @@ using sms_server.Dtos;
 
 [ApiController]
 [Route("api/auth")]
-public class AuthController:ControllerBase
+public class AuthController : ControllerBase
 {
     private readonly IAuthInterface authService;
     public AuthController(IAuthInterface authService)
     {
-      this.authService = authService;
+        this.authService = authService;
     }
 
     [HttpPost("signup")]
-    public async Task<ActionResult<string>> SignUp(AuthDto authDto)
+    public async Task<IActionResult> SignUp(AuthDto authDto)
     {
-      if(string.IsNullOrWhiteSpace(authDto.email))
-      {
-        return BadRequest("Invalid Email");
-      }
-      if(string.IsNullOrWhiteSpace(authDto.password))
-      {
-        return BadRequest("Invalid Password");
-      }
+        try
+        {
 
-      var res = await authService.SignUp(authDto); 
+            if (string.IsNullOrWhiteSpace(authDto.email) || string.IsNullOrWhiteSpace(authDto.email))
+            {
+                return BadRequest("Invalid Email");
+            }
+            if (string.IsNullOrWhiteSpace(authDto.password) || string.IsNullOrWhiteSpace(authDto.password))
+            {
+                return BadRequest("Invalid Password");
+            }
 
-      return Ok(res);
+           var res = await authService.SignUp(authDto);
+            if(res == null)
+            {
+              return Conflict(res);
+            }
+            return Ok(res);
+        } catch(Exception ex)
+        {
+          Console.WriteLine(ex.Message);
+          return StatusCode(500,"An unexpected error occured.");
+        }
     }
 }
