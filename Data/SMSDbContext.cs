0e080ba4e96af8aac99870555c1e808ba01dc2bd
kngval <bkvalteocs@gmail.com>
2024-10-03 14:59:16 +0800

    db context no model builder yet

--- Data/SMSDbContext.cs
+++ Data/SMSDbContext.cs
@@ -7,10 +7,10 @@ public class SMSDbContext:DbContext {
 
   public SMSDbContext(DbContextOptions<SMSDbContext> options) : base(options){}
 
-  public DbSet<UserEntity> User => Set<UserEntity>(); 
+  public DbSet<UserEntity> Users => Set<UserEntity>(); 
   public DbSet<StudentEntity> Student => Set<StudentEntity>(); 
   public DbSet<AdminEntity> Admin => Set<AdminEntity>(); 
   public DbSet<TeacherEntity> Teacher => Set<TeacherEntity>(); 
 
-
+  //Don't forget to put the model builder below _ ;
 }
