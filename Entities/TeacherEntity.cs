5674099fda5467a27847cc1d56f1569b815cd2dd
kngval <bkvalteocs@gmail.com>
2024-10-13 19:08:20 +0800

    removed unnecessary entities

--- Entities/TeacherEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace sms_server.Entities;
-public class TeacherEntity {
-  public int Id {get;set;}
-  public required string Email {get;set;}
-  public required string Password {get;set;}
-  public required string Code {get;set;}
-
-  public string? Name {get;set;}
-}
