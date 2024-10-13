741b40e950267aa6672fe50dab5a6ab3a698b9d6
kngval <bkvalteocs@gmail.com>
2024-10-13 19:08:49 +0800

    adjustments

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using System.Security.Claims;
 //------------------
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
