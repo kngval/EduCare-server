c4f0fae0bfe49f904805f2631905951726421832
kngval <bkvalteocs@gmail.com>
2024-10-16 17:02:27 +0800

    user info service

--- Services/UserInfoService.cs
+++ Services/UserInfoService.cs
@@ -12,12 +12,36 @@ public class UserInfoService:IUserInfoInterface {
 
   }
 
-    public Task<UserInfoResponse> CreateUserInfo(UserInfoDto userInfo)
+    public async Task<UserInfoResponse> CreateUserInfo(UserInfoDto userInfo)
     {
-        throw new NotImplementedException();
+      var user = context.UserInfo.FirstOrDefault(u => u.UserId == userInfo.userId);
+
+      if(user == null)
+      {
+         var newUserInfo = await context.UserInfo.AddAsync(new UserInfoEntity() {
+          FirstName = userInfo.FirstName,
+          MiddleName = userInfo.MiddleName,
+          LastName = userInfo.LastName,
+          Birthdate = userInfo.Birthdate,
+          UserId = userInfo.userId
+        });
+
+        await context.SaveChangesAsync();
+        return new UserInfoResponse()
+        {
+          Success = true,
+          Message = "User Information added successfully !",
+        };
+      } else {
+        user.FirstName = userInfo.FirstName;
+        user.MiddleName = userInfo.MiddleName;
+        user.LastName = userInfo.LastName;
+        user.Birthdate = userInfo.Birthdate;
+        user.UserId = userInfo.userId;
+      }
     }
 
-    public Task<UserInfoResponse> GetUserInfo(int id)
+    public async Task<UserInfoResponse> GetUserInfo(int id)
     {
         throw new NotImplementedException();
     }
