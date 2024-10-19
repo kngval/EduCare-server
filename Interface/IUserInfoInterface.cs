9a38fbb5a689c04c3fe3292d84ddc8635bbb175d
kngval <bkvalteocs@gmail.com>
2024-10-19 13:21:39 +0800

    fetch user

--- Interface/IUserInfoInterface.cs
+++ Interface/IUserInfoInterface.cs
@@ -1,6 +1,6 @@
 
 using sms_server.Dtos;
 public interface IUserInfoInterface {
-  UserInfoEntity? GetUserInfo(int id);
+  UserInfoEntity? FetchUser(int id);
   Task<UserInfoResponse> CreateUserInfo(UserInfoDto userInfo);
 }
