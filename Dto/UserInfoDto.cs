1628de58fdaf9bd1d7c2e77bae54c0a70325b39c
kngval <bkvalteocs@gmail.com>
2024-10-20 21:56:21 +0800

    added role

--- Dto/UserInfoDto.cs
+++ Dto/UserInfoDto.cs
@@ -2,4 +2,4 @@
 
 namespace sms_server.Dtos;
 
-public record UserInfoDto(string FirstName,string MiddleName,string LastName,DateOnly Birthdate,int userId);
+public record UserInfoDto(string FirstName,string MiddleName,string LastName,string Role,DateOnly Birthdate,int userId);
