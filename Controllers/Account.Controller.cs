0a96806e250cee31055c8ca5acf9ce564471d198
kngval <bkvalteocs@gmail.com>
2024-10-20 21:57:35 +0800

    user id retrieval in claimtypes name identifier

--- Controllers/Account.Controller.cs
+++ Controllers/Account.Controller.cs
@@ -1,19 +1,38 @@
 
 
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+[Authorize]
 [ApiController]
 [Route("api/account")]
-public class AccountController:ControllerBase {
-  private readonly IUserInfoInterface userInfoService; 
-  public AccountController(IUserInfoInterface userInfoService)
-  {
-    this.userInfoService = userInfoService;
-  }
-  
-  [HttpGet("/get-userinfo")]
-  public Task<IActionResult> GetUserInfo(int id)
-  {
-    var user = userInfoService.FetchUser(id);
-  }
+public class AccountController : ControllerBase
+{
+    private readonly IUserInfoInterface userInfoService;
+    public AccountController(IUserInfoInterface userInfoService)
+    {
+        this.userInfoService = userInfoService;
+    }
+    [HttpGet("test")]
+    public string Test()
+    {
+        return "Endpoint hit!";
+    }
+
+    [HttpGet("get-userinfo")]
+    public IActionResult GetUserInfo()
+    {
+        var userIdClaimValue = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        if(userIdClaimValue == null)
+        {
+          return BadRequest("Invalid token");
+        }
+        int userId = int.Parse(userIdClaimValue);
+        Console.WriteLine(userId);
+        Console.WriteLine("Hit");
+        var user = userInfoService.FetchUser(userId);
+
+        return Ok(user);
+    }
 }
