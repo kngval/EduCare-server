d57b89479a52e02c0d2868ac07fee9582ef80870
kngval <bkvalteocs@gmail.com>
2024-10-21 18:35:47 +0800

    added new fields

--- Entities/UserInfoEntity.cs
+++ Entities/UserInfoEntity.cs
@@ -3,11 +3,14 @@
 public class UserInfoEntity {
   public int Id {get;set;}
   public string FirstName {get;set;} = string.Empty;
-  public string? MiddleName {get;set;} 
   public string LastName {get;set;} = string.Empty; 
   public required string Role {get;set;} 
+  public string? Phone {get;set;}
   public DateOnly Birthdate {get;set;}
-
+  public string Country {get;set;} = string.Empty;
+  public string State {get;set;} = string.Empty;
+  public string City {get;set;} = string.Empty;
+  public string PostalCode {get;set;} = string.Empty;
   public int UserId {get;set;} //foreign key for UserEntity.Id
 
 }
