d6e586fd71e86e882bd8cd19f813532464a59369
kngval <bkvalteocs@gmail.com>
2024-10-22 21:43:50 +0800

    Action Results

--- Controllers/Account.Controller.cs
+++ Controllers/Account.Controller.cs
@@ -4,6 +4,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using sms_server.Dtos;
 [Authorize]
 [ApiController]
 [Route("api/account")]
@@ -14,22 +15,44 @@ public class AccountController : ControllerBase
     {
         this.userInfoService = userInfoService;
     }
-    [HttpGet("test")]
-    public string Test()
-    {
-        return "Endpoint hit!";
-    }
-
     [HttpGet("get-userinfo")]
     public IActionResult GetUserInfo()
     {
-        var userIdClaimValue = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
-        if(userIdClaimValue == null)
+        var userId = GetUserId();
+        if (userId == null)
         {
-          return BadRequest("Invalid token");
+            return BadRequest("User Id not present in the token.");
         }
-        int userId = int.Parse(userIdClaimValue);
-        var user = userInfoService.FetchUser(userId);
+        var user = userInfoService.FetchUser(userId.Value);
+        if(user == null)
+        {
+          return NotFound($"User with the Id of '{userId.Value}' not found");
+        }
         return Ok(user);
+    }
+    [HttpPost("create-userinfo")]
+    public async Task<IActionResult> CreateUserInfo(UserInfoDto userInfoDto)
+    {
+      var userId = GetUserId(); 
+      if (userId == null)
+      {
+        return BadRequest("User Id not present in the token.");
+      }
+      var res = await userInfoService.CreateUserInfo(userInfoDto,userId.Value);
+      if(res.Success == false){
+        return BadRequest(res);
+      }
+      return Ok(res);
+    }
+    
+    //Method for getting the UserId from the auth token
+    private int? GetUserId()
+    {
+        var userIdClaimValue = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        if (userIdClaimValue == null)
+        {
+            return null;
+        }
+        return int.Parse(userIdClaimValue);
     }
 }
