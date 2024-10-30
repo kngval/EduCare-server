ebfacd7b8ad5f29076a0d7e7af444037380ee2d1
kngval <bkvalteocs@gmail.com>
2024-10-30 16:24:51 +0800

    Random str generator

--- Controllers/Admin.Controller.cs
+++ Controllers/Admin.Controller.cs
@@ -1,4 +1,5 @@
 
+using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 [Authorize(Policy = "admin")]
@@ -9,5 +10,23 @@ public class AdminController:ControllerBase {
   [HttpGet("test")]
   public IActionResult AdminEndpoint(){
     return Ok("Hi User");
+  }
+  
+  [HttpPost("create-code")]
+  public IActionResult CreateCode(){
+      
+  }
+
+  private string GenerateRandomCode(int codeLength){
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
+    StringBuilder randomStrBuilder = new StringBuilder(codeLength);
+    Random random = new Random();
+    for(int i = 0; i < codeLength; i++){
+      int randomIndex = random.Next(alphabet.Length); 
+      char randomChar = alphabet[randomIndex];
+
+      randomStrBuilder.Append(randomChar);
+    }
+    return randomStrBuilder.ToString();
   }
 }
