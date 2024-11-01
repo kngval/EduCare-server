f29a535fc7508a55dc7167a613ab73286d7d8c89
kngval <bkvalteocs@gmail.com>
2024-11-01 14:00:17 +0800

    Code generation

--- Services/AdminService.cs
+++ Services/AdminService.cs
@@ -1,10 +1,44 @@
 
 using System.Text;
+using sms_server.Entities;
+using sms_server.Response;
 
 public class AdminService:IAdminService {
-    public Task GenerateCode(int length)
+    private readonly SMSDbContext context;
+    
+    public AdminService(SMSDbContext context){
+      this.context = context;
+    }
+
+    public AdminResponse GenerateCode(int length)
     {
-        throw new NotImplementedException();
+      if(length <= 0){
+        return new AdminResponse(){
+          Success = false,
+          Message = "Code length not valid."
+        };
+      }
+      var code = GenerateRandomCode(length);
+      
+      var dbCode = context.UserCode.FirstOrDefault(c => c.Code == code);
+      
+      if(dbCode != null){
+        return new AdminResponse(){
+          Success = false ,
+          Message = "Code already exists, generate a new one"
+        };
+      }
+      var newCode = new UserCodeEntity(){
+        Code = code,
+        Available = true
+      };
+      context.UserCode.AddAsync(newCode);
+      context.SaveChangesAsync();
+
+      return new AdminResponse(){
+        Success = true,
+        Message = "Code created successfully !"
+      };
     }
 
     private string GenerateRandomCode(int codeLength){
