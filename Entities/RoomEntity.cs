2fe8caf00417bf9206913337cb013514e33569d0
kngval <bkvalteocs@gmail.com>
2024-11-19 23:28:26 +0800

    added room code

--- Entities/RoomEntity.cs
+++ Entities/RoomEntity.cs
@@ -1,6 +1,7 @@
 
 public class RoomEntity {
   public int Id {get;set;}
+  public required string RoomCode {get;set;}
   public required string  SubjectName {get;set;}
   public int TeacherId {get;set;} //Foreign Key pointing to UserInfo that have a teacher role 
 }
--- Entities/RoomToStudentEntity.cs
+++ Entities/RoomToStudentEntity.cs
@@ -3,5 +3,6 @@ public class RoomToStudentEntity {
   public int Id {get;set;}
   //Foreign Keys
   public int StudentId {get;set;} //FK pointing to UserInfo that have a student role
-  public int RoomId {get;set;} // FK pointing to RoomEntity
+  public int RoomCode {get;set;} // FK pointing to RoomEntity
+  public int Grade {get;set;}
 }
