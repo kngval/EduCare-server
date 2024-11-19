3488584f7876e4afcc225cdbfb4e4f250e56bce7
kngval <bkvalteocs@gmail.com>
2024-11-19 23:30:15 +0800

    random str gen

---
+++ Helpers/Helpers.cs
@@ -0,0 +1,21 @@
+
+using System.Text;
+
+public static class Helpers {
+
+public static string GenerateRandomCode(int codeLength)
+{
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    StringBuilder randomStrBuilder = new StringBuilder(codeLength);
+    Random random = new Random();
+    for (int i = 0; i < codeLength; i++)
+    {
+        int randomIndex = random.Next(alphabet.Length);
+        char randomChar = alphabet[randomIndex];
+
+        randomStrBuilder.Append(randomChar);
+    }
+    return randomStrBuilder.ToString();
+}
+} 
+
