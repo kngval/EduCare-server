07f4acc4dbf2b1a8708ef45d5074345ce432837d
kngval <bkvalteocs@gmail.com>
2024-11-20 20:31:53 +0800

    fetch userId from token

--- Helpers/Helpers.cs
+++ Helpers/Helpers.cs
@@ -1,21 +1,24 @@
 
 using System.Text;
 
-public static class Helpers {
-
-public static string GenerateRandomCode(int codeLength)
+public static class Helpers
 {
-    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    StringBuilder randomStrBuilder = new StringBuilder(codeLength);
-    Random random = new Random();
-    for (int i = 0; i < codeLength; i++)
-    {
-        int randomIndex = random.Next(alphabet.Length);
-        char randomChar = alphabet[randomIndex];
 
-        randomStrBuilder.Append(randomChar);
+    public static string GenerateRandomCode(int codeLength)
+    {
+        string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        StringBuilder randomStrBuilder = new StringBuilder(codeLength);
+        Random random = new Random();
+        for (int i = 0; i < codeLength; i++)
+        {
+            int randomIndex = random.Next(alphabet.Length);
+            char randomChar = alphabet[randomIndex];
+
+            randomStrBuilder.Append(randomChar);
+        }
+        return randomStrBuilder.ToString();
     }
-    return randomStrBuilder.ToString();
+
+
 }
-} 
 
