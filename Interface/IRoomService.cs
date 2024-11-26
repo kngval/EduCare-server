f2b75bf1de053fb522fdeafa9a48adeb16eeabb6
kngval <bkvalteocs@gmail.com>
2024-11-26 20:12:27 +0800

    changed return type

--- Interface/IRoomService.cs
+++ Interface/IRoomService.cs
@@ -1,7 +1,7 @@
 
 public interface IRoomService {
   List<RoomEntity> FetchRooms(); 
-  RoomEntity FetchRoomDetails(int id);
+  RoomEntity? FetchRoomDetails(int id);
   CreateRoomResponse CreateRoom(RoomDto roomDto); 
   CreateRoomResponse DeleteRoom(int id);
 }
