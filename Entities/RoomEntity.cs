beae69007ecc6e60507852433f349b909f6ba620
kngval <bkvalteocs@gmail.com>
2024-11-28 21:46:07 +0800

    changed props

--- Entities/RoomEntity.cs
+++ Entities/RoomEntity.cs
@@ -3,5 +3,5 @@ public class RoomEntity {
   public int Id {get;set;}
   public required string RoomCode {get;set;}
   public required string  SubjectName {get;set;}
-  public int TeacherId {get;set;} //Foreign Key pointing to UserInfo that have a teacher role 
+  public int? TeacherId {get;set;} //Foreign Key pointing to UserInfo that have a teacher role 
 }
