23d7a2ac8beb57de2ebb0542a64114d6fa363884
kngval <bkvalteocs@gmail.com>
2024-11-28 21:46:18 +0800

    changed params

--- Interface/IRoomService.cs
+++ Interface/IRoomService.cs
@@ -2,7 +2,7 @@
 public interface IRoomService {
   List<RoomEntity> FetchRooms(); 
   RoomEntity? FetchRoomDetails(int id);
-  CreateRoomResponse CreateRoom(RoomDto roomDto); 
+  CreateRoomResponse CreateRoom(string roomName); 
   CreateRoomResponse DeleteRoom(int id);
 }
 
