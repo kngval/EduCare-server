f42321f25278cce0ebe2d8920b14ab1e6805be86
kngval <bkvalteocs@gmail.com>
2024-12-09 17:24:08 +0800

    nullable teacherId? teacherName? from RoomEntity

---
+++ Migrations/20241209084543_TeacherName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class TeacherName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TeacherName",
+                table: "Rooms",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeacherName",
+                table: "Rooms");
+        }
+    }
+}
