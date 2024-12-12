f1ecce2aa1a5ab9b1db682dc6ca2a9287f1e6702
kngval <bkvalteocs@gmail.com>
2024-12-12 22:44:07 +0800

    fetch rooms for all users

--- Interface/IRoomService.cs
+++ Interface/IRoomService.cs
@@ -1,6 +1,7 @@
 
 public interface IRoomService {
   List<RoomEntity> AdminFetchRooms(); 
+  List<RooomEntity> FetchRooms(int userId)
   RoomEntity? FetchRoomDetails(int id);
   CreateRoomResponse CreateRoom(RoomDto roomDto); 
   CreateRoomResponse DeleteRoom(int id);
