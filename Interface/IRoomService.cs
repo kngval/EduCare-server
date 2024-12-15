fa798138f4bce5f0d12bf87e9c8bc3b9feb235dc
kngval <bkvalteocs@gmail.com>
2024-12-15 12:26:29 +0800

    join room method

--- Interface/IRoomService.cs
+++ Interface/IRoomService.cs
@@ -4,6 +4,7 @@ public interface IRoomService {
   List<RoomEntity> FetchRooms(int userId);
   RoomEntity? FetchRoomDetails(int id);
   CreateRoomResponse CreateRoom(RoomDto roomDto); 
+  CreateRoomResponse JoinRoom(string roomCode,int studentId);
   CreateRoomResponse DeleteRoom(int id);
 }
 
