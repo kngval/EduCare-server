74aaa2a0e70e759d8395a91dc90b343230ead7d6
kngval <bkvalteocs@gmail.com>
2024-12-16 16:11:12 +0000

    nullable id

---
+++ Migrations/20241216160900_nullableId.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class nullableId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RoomCode",
+                table: "RoomsToStudent",
+                newName: "RoomId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Grade",
+                table: "RoomsToStudent",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RoomId",
+                table: "RoomsToStudent",
+                newName: "RoomCode");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Grade",
+                table: "RoomsToStudent",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
