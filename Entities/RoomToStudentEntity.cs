4967a7127a3d845e15323be22f8aa901e897931e
kngval <bkvalteocs@gmail.com>
2024-12-17 18:57:38 +0000

    join roomEnt

--- Entities/RoomToStudentEntity.cs
+++ Entities/RoomToStudentEntity.cs
@@ -5,4 +5,6 @@ public class RoomToStudentEntity {
   public int StudentId {get;set;} //FK pointing to UserInfo that have a student role
   public int RoomId {get;set;} // FK pointing to RoomEntity
   public int? Grade {get;set;}
+
+  public RoomEntity? Room{get;set;}
 }
