b28af5e2f1e0d898bc37b8c3f35422fe71a4cccf
kngval <bkvalteocs@gmail.com>
2024-12-19 22:45:17 +0000

    added fk pointer to =>  UserInfo table

---
+++ Migrations/20241219215624_FK UserInfo.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FKUserInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserInfoId",
+                table: "RoomsToStudent",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomsToStudent_RoomId",
+                table: "RoomsToStudent",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomsToStudent_UserInfoId",
+                table: "RoomsToStudent",
+                column: "UserInfoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoomsToStudent_Rooms_RoomId",
+                table: "RoomsToStudent",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoomsToStudent_UserInfo_UserInfoId",
+                table: "RoomsToStudent",
+                column: "UserInfoId",
+                principalTable: "UserInfo",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoomsToStudent_Rooms_RoomId",
+                table: "RoomsToStudent");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoomsToStudent_UserInfo_UserInfoId",
+                table: "RoomsToStudent");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RoomsToStudent_RoomId",
+                table: "RoomsToStudent");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RoomsToStudent_UserInfoId",
+                table: "RoomsToStudent");
+
+            migrationBuilder.DropColumn(
+                name: "UserInfoId",
+                table: "RoomsToStudent");
+        }
+    }
+}
