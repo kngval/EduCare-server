908d3fd829cd822a28f850b8a70e508bf1660e39
kngval <bkvalteocs@gmail.com>
2024-12-21 18:05:40 +0000

    fixed args

--- Interface/IRoomService.cs
+++ Interface/IRoomService.cs
@@ -2,7 +2,7 @@
 public interface IRoomService {
   List<RoomEntity> AdminFetchRooms(); 
   List<RoomToStudentEntity> FetchRooms(int userId);
-  RoomEntity? FetchRoomDetails(int id);
+  RoomEntity? FetchRoomDetails(int roomId,int userId);
   CreateRoomResponse CreateRoom(RoomDto roomDto); 
   CreateRoomResponse JoinRoom(int studentId,string roomCode);
   CreateRoomResponse DeleteRoom(int id);
