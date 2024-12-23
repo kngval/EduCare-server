bd14c35056a94cec40686f641a5c5a1d3ffce659
kngval <bkvalteocs@gmail.com>
2024-12-23 10:37:35 -0400

    added role

--- Entities/RoomToStudentEntity.cs
+++ Entities/RoomToStudentEntity.cs
@@ -4,8 +4,9 @@ public class RoomToStudentEntity {
   //Foreign Keys
   public int StudentId {get;set;} //FK pointing to UserInfo that have a student role
   public int RoomId {get;set;} // FK pointing to RoomEntity
+  public int UserInfoId {get;set;} //FK pointing to UserInfo
   public int? Grade {get;set;}
-
+  public string? Role {get;set;}
   public RoomEntity? Room{get;set;}
   public UserInfoEntity? UserInfo {get;set;}
 }
