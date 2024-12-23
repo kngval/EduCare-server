662f255a3c545ac08fc90a444197a6b83f6a06c8
kngval <bkvalteocs@gmail.com>
2024-12-23 10:37:45 -0400

    fetch room details

--- Interface/IRoomService.cs
+++ Interface/IRoomService.cs
@@ -3,6 +3,7 @@ public interface IRoomService {
   List<RoomEntity> AdminFetchRooms(); 
   List<RoomToStudentEntity> FetchRooms(int userId);
   RoomEntity? FetchRoomDetails(int roomId,int userId);
+  RoomToStudentEntity? FetchRoomsStudent(int roomId);
   CreateRoomResponse CreateRoom(RoomDto roomDto); 
   CreateRoomResponse JoinRoom(int studentId,string roomCode);
   CreateRoomResponse DeleteRoom(int id);
