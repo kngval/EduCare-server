f44f960cdb6f7513e6160d4cc1a31dccea4d391b
kngval <bkvalteocs@gmail.com>
2024-12-27 19:30:47 +0800

    fetch studs

--- Interface/IRoomService.cs
+++ Interface/IRoomService.cs
@@ -4,7 +4,7 @@ public interface IRoomService {
   List<RoomEntity> TeacherFetchRooms(int userId);
   List<RoomToStudentEntity> FetchRooms(int userId);
   RoomEntity? FetchRoomDetails(int roomId,int userId);
-  RoomToStudentEntity? FetchRoomsStudent(int roomId);
+  List<RoomToStudentEntity>? FetchRoomsStudent(int roomId,int userId);
   CreateRoomResponse CreateRoom(RoomDto roomDto,int userId); 
   CreateRoomResponse JoinRoom(int studentId,string roomCode);
   CreateRoomResponse DeleteRoom(int id);
