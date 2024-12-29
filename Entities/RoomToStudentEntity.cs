b430e2509f0f1a69e04f045aea9c5490f3ead976
kngval <bkvalteocs@gmail.com>
2024-12-29 20:27:50 +0800

    prof

--- Entities/RoomToStudentEntity.cs
+++ Entities/RoomToStudentEntity.cs
@@ -3,6 +3,7 @@ public class RoomToStudentEntity {
   public int Id {get;set;}
   //Foreign Keys
   public int StudentId {get;set;} //FK pointing to UserInfo that have a student role
+  public string? Email {get;set;}
   public int RoomId {get;set;} // FK pointing to RoomEntity
   public int UserInfoId {get;set;} //FK pointing to UserInfo
   public int? Grade {get;set;}
