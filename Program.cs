efbff97f80d717e9186f7654eaf01b7a94a997e2
kngval <bkvalteocs@gmail.com>
2025-01-03 12:23:04 +0800

    imports

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-
 //------------------
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
