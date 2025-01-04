e753fb0ab359ddb9dc7f5715967aa06336af1398
kngval <bkvalteocs@gmail.com>
2025-01-04 18:46:18 +0800

    grade studs

---
+++ Services/Grades.Service.cs
@@ -0,0 +1,55 @@
+
+
+
+public class GradesService : IGradesInterface
+{
+    private readonly SMSDbContext context;
+
+    public GradesService(SMSDbContext context)
+    {
+        this.context = context;
+    }
+    public List<RoomToStudentEntity> FetchStudentGrades(int studentId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public GradingResponse GradeStudent(GradesRequest gradesRequest)
+    {
+        if (gradesRequest == null || gradesRequest.studentId <= 0 || gradesRequest.roomId <= 0)
+        {
+            return new GradingResponse
+            {
+                Success = false,
+                Message = "Invalid request data"
+            };
+        }
+        var studentRooms = context.RoomsToStudent.Where(r => r.StudentId == gradesRequest.studentId);
+        if (studentRooms == null)
+        {
+            return new GradingResponse
+            {
+                Success = false,
+                Message = "Student does not exist"
+            };
+        }
+        var studentGrade = studentRooms.FirstOrDefault(r => r.RoomId == gradesRequest.roomId);
+        if (studentGrade == null)
+        {
+            return new GradingResponse
+            {
+                Success = false,
+                Message = "Room does not exist"
+            };
+        }
+
+        studentGrade.Grade = gradesRequest.grade;
+        context.SaveChanges();
+
+        return new GradingResponse
+        {
+            Success = true,
+            Message = "Student graded successfully"
+        };
+    }
+}
