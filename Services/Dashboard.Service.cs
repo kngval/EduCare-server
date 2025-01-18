d8eb5adf2405bd47ede5e0f9e42af23694767475
kngval <bkvalteocs@gmail.com>
2025-01-18 13:52:29 +0800

    dashboard fetch and create from db

--- Services/Dashboard.Service.cs
+++ Services/Dashboard.Service.cs
@@ -1,12 +1,70 @@
+public class DashboardService
+{
+    private readonly SMSDbContext context;
+    public DashboardService(SMSDbContext context)
+    {
+        this.context = context;
+    }
+
+    public List<DashboardEntity> FetchAnnouncements()
+    {
+        return context.Dashboard.ToList();
+    }
+
+    public DashboardResponse PostAnnouncement(DashboardDto dashboardDto)
+    {
+        if (string.IsNullOrEmpty(dashboardDto.Title) || string.IsNullOrWhiteSpace(dashboardDto.Title))
+        {
+            return new DashboardResponse
+            {
+                Success = false,
+                Message = "Title is required"
+            };
+        }
 
 
-public class DashboardService {
+        if (string.IsNullOrEmpty(dashboardDto.Message) || string.IsNullOrWhiteSpace(dashboardDto.Message))
+        {
+            return new DashboardResponse
+            {
+                Success = false,
+                Message = "Message is required"
+            };
+        }
 
-  private readonly SMSDbContext context;
-  public DashboardService(SMSDbContext context)
-  {
-    this.context = context;
-  }
+        DashboardEntity newDashboard = new DashboardEntity()
+        {
+          Title = dashboardDto.Title,
+          Message = dashboardDto.Message,
+          Date = DateOnly.FromDateTime(DateTime.Now)
+        };
 
+        context.Dashboard.Add(newDashboard);
+        context.SaveChanges();
 
+        return new DashboardResponse {
+          Success = true,
+          Message = "Dashboard Updated",
+          Dashboard = newDashboard
+        };
+    }
+
+    public DashboardResponse DeleteAnnouncement(int annId)
+    {
+      var announcement = context.Dashboard.Find(annId);
+      if(announcement == null)
+      {
+        return new DashboardResponse {
+          Success = false,
+          Message = "Announcement id not found in the database"
+        };
+      };
+
+      context.Dashboard.Remove(announcement);
+      return new DashboardResponse {
+        Success = true,
+        Message = "Post removed successfully !"
+      };
+    }
+    
 }
