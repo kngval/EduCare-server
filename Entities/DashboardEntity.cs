c2de0620189f223d8ddac36f3eff434de0879913
kngval <bkvalteocs@gmail.com>
2025-01-18 13:51:24 +0800

    db entity

---
+++ Entities/DashboardEntity.cs
@@ -0,0 +1,7 @@
+
+public class DashboardEntity {
+  public int Id {get;set;}
+  public required string Title {get;set;} 
+  public required string Message {get;set;}
+  public DateOnly Date {get;set;}
+}
