3698c6092b26076ddb70aeadec3daff326d2304f
kngval <bkvalteocs@gmail.com>
2025-01-19 19:54:17 +0800

    dashboard migration

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 //------------------
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
