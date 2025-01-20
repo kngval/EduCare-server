b1a8e1a2bb125c1e0fcc4eaf6e305d73744a5411
kngval <bkvalteocs@gmail.com>
2025-01-20 21:07:09 +0800

    updated Date to => string

--- Entities/DashboardEntity.cs
+++ Entities/DashboardEntity.cs
@@ -3,5 +3,5 @@ public class DashboardEntity {
   public int Id {get;set;}
   public required string Title {get;set;} 
   public required string Message {get;set;}
-  public DateOnly Date {get;set;}
+  public required string Date {get;set;}
 }
